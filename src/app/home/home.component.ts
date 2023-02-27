7d9f9ad738a42292785072e5d1805033d37f662e
鄭至辰 <tom035370@gmail.com>
2023-02-27 16:04:49 +0800

    c cs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,3 +8,17 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
 }
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MyService {
+  constructor(private http: HttpClient) {}
+
+  postData(data: any) {
+    const url = 'http://localhost:3000/my-endpoint';
+    return this.http.post(url, data);
+  }
+}
